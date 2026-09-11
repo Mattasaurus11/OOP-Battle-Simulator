c69fb20775c7e1263f626387e1d8d70c3957047f
Matthew <keepersmatthew@gmail.com>
2026-09-11 12:52:47 -0500

    Add second goblin entrance

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 from goblin import Goblin
 
 
-ARENA_NAME = "The Iron Lung"
+ARENA_NAME = "Dimensional Crack"
 
 
 def main():
@@ -10,9 +10,11 @@ def main():
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
 
-    goblin = Goblin("Gribble")
+    goblin = Goblin("Beast Monarch")
+    goblin2 = Goblin("Insect Monarch")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+    print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
     print("But no hero has answered the call... yet.")
 
 
