108b70f506749d58352bf869208f57b75b4850aa
Matthew <keepersmatthew@gmail.com>
2026-09-15 09:55:48 -0500

    Update game.py
    Added a better battle scenario

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from monster import Monster
 from hunter import Hunter
+from time import sleep
 
 ARENA_NAME = "Dimensional Crack"
 
@@ -10,24 +11,55 @@ def main():
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gate is opening...")
 
+    enemies = []
     monster = Monster("Beast Monarch")
+    enemies.append(monster.name)
     monster2 = Monster("Insect Monarch")
+    enemies.append(monster2.name)
     monster3 = Monster("Frost Monarch")
+    enemies.append(monster3.name)
+    sleep(0.5)
 
-    hunter = Hunter("Sung Jinwoo (Shadow Monarch)")
+    
 
     print(f"{monster.name} enters the dungeon with {monster.health} health.")
     print(f"{monster2.name} enters the dungeon with {monster2.health} health.")
     print(f"{monster3.name} enters the dungeon with {monster3.health} health.")
     print("But no hunter has answered the call... yet.")
+    sleep(0.5)
 
-    print(f"{hunter.name} has answered the call and now enters the dungeon with {hunter.health} health.")
+    hunter = Hunter(input("What is the Hunters Name: "))
+    sleep(0.5)
 
-    attackedMonsterHealth = monster.take_damage(hunter.attack())
-    if monster.is_alive():
-        print("Since monster is still alive it attacks hunter")
-        attackedHunterHealth = hunter.take_damage(monster.attack())
-
+    while hunter.is_alive():
+            if monster.is_alive():
+                attackedMonsterHealth = monster.take_damage(hunter.attack())
+                sleep(0.5)
+                print("Since monster is still alive it attacks hunter")
+                sleep(0.5)
+                attackedHunterHealth = hunter.take_damage(monster.attack())
+                sleep(0.5)
+            else:
+                if monster2.is_alive():
+                    attackedMonsterHealth = monster2.take_damage(hunter.attack())
+                    sleep(0.5)
+                    print("Since monster is still alive it attacks hunter")
+                    sleep(0.5)
+                    attackedHunterHealth = hunter.take_damage(monster2.attack())
+                    sleep(0.5)
+                else:
+                    if monster3.is_alive():
+                        attackedMonsterHealth = monster3.take_damage(hunter.attack())
+                        sleep(0.5)
+                        print("Since monster is still alive it attacks hunter")
+                        sleep(0.5)
+                        attackedHunterHealth = hunter.take_damage(monster3.attack())
+                        sleep(0.5)
+                    else:
+                        print(f"Hunter has won the battle against {enemies}")
+                        sleep(0.5)
+                        return True
+            print(f"{enemies} has won the battle")
 
 if __name__ == "__main__":
     main()
